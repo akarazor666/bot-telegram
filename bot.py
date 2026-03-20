ea4beddb4bcc18b4121c15c4aa158ec3701b5330
akarazor666 <akarazor666@gmail.com>
2026-03-20 10:29:33 -0400

    função de deletar comando corrigida 2.0

--- bot.py
+++ bot.py
@@ -430,6 +430,17 @@ async def delete_commands(update, context):
             await update.message.delete()
         except:
             pass
+import asyncio  # se ainda não tiver importado
+
+async def delete_command_delayed(update, context):
+    # só tenta apagar se for mensagem de texto começando com /
+    if update.message and update.message.text.startswith("/"):
+        # espera 2 segundos
+        await asyncio.sleep(2)
+        try:
+            await update.message.delete()
+        except:
+            pass
 
 # ---------------- RUN ----------------
 
@@ -453,6 +464,7 @@ app.add_handler(CommandHandler("setgoodbyeimg", setgoodbyeimg))
 app.add_handler(CommandHandler("previewgoodbye", previewgoodbye))
 app.add_handler(MessageHandler(filters.StatusUpdate.LEFT_CHAT_MEMBER, goodbye))
 app.add_handler(MessageHandler(filters.TEXT & filters.Regex(r"^/"), delete_commands))
+app.add_handler(MessageHandler(filters.TEXT & filters.Regex(r"^/"), delete_command_delayed))
 
 print("🔥 Bot estilo Rose rodando...")
 app.run_polling()
